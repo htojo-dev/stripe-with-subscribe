3b17dffd5e80292382322fc5af298d2ac67b4def
htojo-dev <web.h.tojo1209@gmail.com>
2025-05-14 15:00:54 +0900

    分割したprofile-client.tsを使用するようAccountFormを修正

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,76 +1,10 @@
-"use client";
-
-import { getProfileData } from "@/lib/supabase/profile/profile-client";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import AccountForm from "@/components/ui/AccountForm";
 
 const Account = () => {
-  const router = useRouter();
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [canChangeEmail, setCanChangeEmail] = useState(false);
-  const [loading, setLoading] = useState(true);
-
-  // useSWRはキャッシュを前提としていて反映にラグが出やすいため、useEffectで処理
-  useEffect (() => {
-    const fetchData = async () => {
-      try {
-        const result = await getProfileData({redirectOnFail: false})
-
-        if(!result) {
-          router.push('/login');
-          return;
-        }
-
-        const {profile, user} = result;
-
-        setName(profile.name || "");
-        setEmail(profile.email || "");
-        setCanChangeEmail(user.app_metadata?.provider === "email");
-
-        setLoading(false);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-
-    fetchData();
-  }, [router])
-
-  if (loading) {
-    return <p>読み込み中...</p>;
-  }
-
   return (
     <div className="w-full max-w-3xl mx-auto my-16 px-2">
-      <form>
-        <div>
-          <label htmlFor="name">Name :</label>
-          <input
-            type="text"
-            id="name"
-            className="w-full bg-gray-100 py-2 px-4 rounded-xs cursor-pointer"
-            name="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email :</label>
-          {canChangeEmail ? (
-            <input
-              type="email"
-              id="email"
-              className="w-full bg-gray-100 py-2 px-4 rounded-xs cursor-pointer"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          ) : (
-            <p>{email}</p>
-          )}
-        </div>
-      </form>
+      <h2 className="mb-10">ユーザープロフィール画面</h2>
+      <AccountForm />
     </div>
   );
 };
---
+++ components/ui/AccountForm.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { getProfileData } from "@/lib/supabase/profile/profile-client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+const AccountForm = () => {
+  const router = useRouter();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [canChangeEmail, setCanChangeEmail] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  // useSWRはキャッシュを前提としていて反映にラグが出やすいため、useEffectで処理
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await getProfileData({ redirectOnFail: false });
+
+        if (!result) {
+          router.push("/login");
+          return;
+        }
+
+        const { profile, user } = result;
+
+        setName(profile.name || "");
+        setEmail(profile.email || "");
+        setCanChangeEmail(user.app_metadata?.provider === "email");
+
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setErrorMsg("情報の取得中に問題が発生しました")
+      }
+    };
+
+    fetchData();
+  }, [router]);
+
+  if (loading) {
+    return <p>読み込み中...</p>;
+  }
+
+  return (
+    <form>
+      {errorMsg && <p className="text-red-500 mb-4">{errorMsg}</p>}
+      <div>
+        <label htmlFor="name">Name :</label>
+        <input
+          type="text"
+          id="name"
+          className="w-full bg-gray-100 py-2 px-4 rounded-xs cursor-pointer"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email :</label>
+        {canChangeEmail ? (
+          <input
+            type="email"
+            id="email"
+            className="w-full bg-gray-100 py-2 px-4 rounded-xs cursor-pointer"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        ) : (
+          <p>{email}</p>
+        )}
+      </div>
+    </form>
+  );
+};
+
+export default AccountForm;
